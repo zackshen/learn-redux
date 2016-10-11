20869b7a15a3c25f6a6f2b66c54cbf062683d24c
zack.shen <zack.shen@netis.com.cn>
2016-10-11 17:00:41 +0800

    add class_3: 将三个reducer组合成一颗状态树

--- class_3.js
+++ class_3.js
@@ -1,5 +1,5 @@
 /*
- * 如何使用多个reducer
+ * 如何形成一颗状态树
  */
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { logDispatch, logSubscribe } from './utils';
@@ -32,23 +32,48 @@ function stringReducer(state = 'hello', action) {
 }
 
 /*
- * 将多个reducer组合成一个reducer
+ * 起名为rootSliceReducer是为了和下面的childReducers区分开来
+ * 好辨别, 可以不那么取名字的
  */
-let reducers = combineReducers({
+function rootSliceReducer(state = 'redux', action) {
+    let { size, str } = action;
+    size = size || 2;
+    str = str || state;
+
+    if (action.type === 'left') {
+        return str.slice(0, size);
+    } else if (action.type == 'right') {
+        return str.slice(str.length - size, str.length);
+    }
+    return str;
+}
+
+/*
+ * 将number和string的reducer组合起来
+ */
+let childReducers = combineReducers({
     'number': numberReducer,
     'string': stringReducer
 });
 
 /*
- * 创建一个store实例, 注册刚才创建的组合reducer
+ * 将slice的reducer和上面的childReducer组合起来
+ * 这样就形成了一颗想象中的状态树
+ */
+let reducers = combineReducers({
+    'root': rootSliceReducer,
+    'child': childReducers
+});
+
+/*
+ * 还是将reducer注册到store中
  */
 let store = createStore(reducers);
 let dispatch = logDispatch(store);
 logSubscribe(store);
 
-/*
- * 派发各种action
- */
 dispatch({'type': 'increment'});
 dispatch({'type': 'increment'});
 dispatch({'type': 'increment'});
+dispatch({'type': 'upper'});
+dispatch({'type': 'upper', 'string': 'abc'});
